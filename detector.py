6ee1b3ddbec84b09c22aa07eeaab4823d84ffc9d
SECTION <sec117tion@gmail.com>
2022-05-13 10:47:45 +0530

    refactored

--- detector.py
+++ detector.py
@@ -1,7 +1,3 @@
-'''
-Implement and test car detection (localization)
-'''
-
 import numpy as np
 import tensorflow as tf
 tf = tf.compat.v1
@@ -12,10 +8,6 @@ import time
 from glob import glob
 cwd = os.path.dirname(os.path.realpath(__file__))
 
-# Uncomment the following two lines if need to use the Tensorflow visualization_unitls
-#os.chdir(cwd+'/models')
-#from object_detection.utils import visualization_utils as vis_util
-
 class CarDetector(object):
     def __init__(self):
 
@@ -23,22 +15,14 @@ class CarDetector(object):
         
         os.chdir(cwd)
         
-        #Tensorflow localization/detection model
-        # Single-shot-dectection with mobile net architecture trained on COCO dataset
-        
         detect_model_name = 'ssd_mobilenet_v1_coco_11_06_2017'
         
         PATH_TO_CKPT = detect_model_name + '/frozen_inference_graph.pb'
-        
-        # setup tensorflow graph
+
         self.detection_graph = tf.Graph()
-        
-        # configuration for possible GPU use
+
         config = tf.ConfigProto()
-        #config = tf.compat.v1.ConfigProto()
         config.gpu_options.allow_growth = True
-        # load frozen tensorflow detection model and initialize 
-        # the tensorflow graph
         with self.detection_graph.as_default():
             od_graph_def = tf.GraphDef()
             with tf.gfile.GFile(PATH_TO_CKPT, 'rb') as fid:
@@ -48,37 +32,22 @@ class CarDetector(object):
                
             self.sess = tf.Session(graph=self.detection_graph, config=config)
             self.image_tensor = self.detection_graph.get_tensor_by_name('image_tensor:0')
-              # Each box represents a part of the image where a particular object was detected.
             self.boxes = self.detection_graph.get_tensor_by_name('detection_boxes:0')
-              # Each score represent how level of confidence for each of the objects.
-              # Score is shown on the result image, together with the class label.
             self.scores =self.detection_graph.get_tensor_by_name('detection_scores:0')
             self.classes = self.detection_graph.get_tensor_by_name('detection_classes:0')
             self.num_detections =self.detection_graph.get_tensor_by_name('num_detections:0')
-    
-    # Helper function to convert image into numpy array    
+
     def load_image_into_numpy_array(self, image):
          (im_width, im_height) = image.size
          return np.array(image.getdata()).reshape(
-            (im_height, im_width, 3)).astype(np.uint8)       
-    # Helper function to convert normalized box coordinates to pixels
+            (im_height, im_width, 3)).astype(np.uint8)
     def box_normal_to_pixel(self, box, dim):
     
         height, width = dim[0], dim[1]
         box_pixel = [int(box[0]*height), int(box[1]*width), int(box[2]*height), int(box[3]*width)]
         return np.array(box_pixel)       
         
-    def get_localization(self, image, visual=False):  
-        
-        """Determines the locations of the cars in the image
-
-        Args:
-            image: camera image
-
-        Returns:
-            list of bounding boxes: coordinates [y_up, x_left, y_down, x_right]
-
-        """
+    def get_localization(self, image, visual=False):
         category_index={1: {'id': 1, 'name': u'person'},
                         2: {'id': 2, 'name': u'bicycle'},
                         3: {'id': 3, 'name': u'car'},
@@ -148,7 +117,6 @@ class CarDetector(object):
         return self.car_boxes
         
 if __name__ == '__main__':
-        # Test the performance of the detector
         det =CarDetector()
         os.chdir(cwd)
         TEST_IMAGE_PATHS= glob(os.path.join('test_images/', '*.jpg'))
